12849af08f0e28f4a3621165a3f1177541b65e3c
suman <sumanghosh8767@gmail.com>
2025-10-07 01:12:54 +0530

    .

--- lambda.py
+++ lambda.py
@@ -9,3 +9,22 @@
 # a=odd_even(11)
 # print(a)
 
+# 1. Multiply three numbers
+multiply = lambda a, b, c: a * b * c
+print(multiply(2, 3, 4))
+
+# 2. Square of a number
+square = lambda x: x * x
+print(square(4))
+
+# 3. Find length of a string
+length = lambda s: len(s)
+print(length("Lambda"))
+
+# 4. Convert Celsius to Fahrenheit
+c_to_f = lambda c: (c * 9/5) + 32
+print(c_to_f(0))
+
+# 5. Reverse a string
+reverse = lambda s: s[::-1]
+print(reverse("Python"))
---
+++ map_filter_reduce.py
@@ -0,0 +1,6 @@
+# 6/10/25
+
+l=[1,2,3,4]
+res=map(lambda x:x+l,l)
+print(res)
+print(type(res))
